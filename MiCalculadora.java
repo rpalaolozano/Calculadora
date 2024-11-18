3dfa3d7928d0b4d2bcc26a697b32e5d3434307d6
r.palaolozano <128861590+rpalaolozano@users.noreply.github.com>
2024-11-18 17:48:35 +0100

    Actualizar MiCalculadora.java

--- MiCalculadora.java
+++ MiCalculadora.java
@@ -3,7 +3,7 @@ public class MiCalculadora {
     // Este es el método principal, el punto de entrada del programa
     public static void main(String[] args) {
 
-        int suma = 5 + 5;
+        int suma = 10 + 10;
 
     }
 }
