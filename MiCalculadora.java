fd9f7fc94286c61d970ede1a9680fbf1a2bcdf26
RaulPalao <r.palaolozano@edu.gva.es>
2024-11-18 16:43:27 +0100

    añado fichero calculadora

---
+++ MiCalculadora.java
@@ -0,0 +1,9 @@
+public class MiCalculadora {
+
+    // Este es el método principal, el punto de entrada del programa
+    public static void main(String[] args) {
+
+        int suma = 5 + 5;
+
+    }
+}
