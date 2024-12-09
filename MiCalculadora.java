f54cedcb64a732bffc75180e2619a9816b00d6c4
raul <r.palaolozano@edu.gva.es>
2024-12-09 16:48:44 +0100

    Añado division

--- MiCalculadora.java
+++ MiCalculadora.java
@@ -6,9 +6,12 @@ public class MiCalculadora {
         int suma = 15 + 15;
         int resta = 10-10;
         int multiplicacion = 5*5;
+        float division = 5/5;
         System.out.println(suma);
         System.out.println(resta);
-        System.out.println(multiplicacion);
+        System.out.println(multiplicacion);        
+        System.out.println(division);
+
 
     }
 }
